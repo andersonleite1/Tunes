858d835b5d123a8beb8e9026c5647332a9bb7f5e
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:49:27 -0300

    feat: create page Album

---
+++ src/pages/Album.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Album() {
+  return (
+    <Container data-testid="page-album">
+      <h1>Album</h1>
+    </Container>
+  );
+}
