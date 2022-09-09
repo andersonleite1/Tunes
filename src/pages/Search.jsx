87555616ed2b279e32cf138deae166abe044b76b
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:49:07 -0300

    feat: create page Search

---
+++ src/pages/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Search() {
+  return (
+    <Container data-testid="page-search">
+      <h1>Search</h1>
+    </Container>
+  );
+}
