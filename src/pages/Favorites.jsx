48e1d96e477d5f9756d8a234cd9683b0c693a7f3
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:50:02 -0300

    feat: create page Favorites

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Favorites() {
+  return (
+    <Container data-testid="page-favorites">
+      <h1>Favorites</h1>
+    </Container>
+  );
+}
