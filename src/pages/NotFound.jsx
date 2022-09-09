1c28d694c6a603bd034aaf9477a1d94debfde669
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:51:07 -0300

    feat: create page NotFound

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function NotFound() {
+  return (
+    <Container data-testid="page-not-found">
+      <h1>Page Not Found</h1>
+    </Container>
+  );
+}
