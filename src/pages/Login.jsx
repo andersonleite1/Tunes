2131db38afb9171e7c8e6536a534c85668a571bf
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:48:48 -0300

    feat: create page Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Login() {
+  return (
+    <Container data-testid="page-login">
+      <h1>Login</h1>
+    </Container>
+  );
+}
