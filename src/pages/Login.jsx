deed70f811f13947840c74d6f65e21edf3460420
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 16:24:55 -0300

    feat: add page Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,80 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, Form, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+
+import { createUser } from '../services/userAPI';
+
+import logo from '../images/logo.png';
+
+const Image = styled.img`
+    max-width: 100%;
+    width: 14rem;
+    margin: 0 auto;
+  `;
+
+const Div = styled.div`
+    background-color: #fafafa;
+    width: 100vw;
+    height: 100vh;
+  `;
+
+const rowStyle = {
+  margin: '0 auto',
+  marginTop: '4rem',
+  maxWidth: '35rem',
+  borderRadius: '20px',
+  boxShadow: ' 2px 3px #b8b8b88d',
+
+};
+
+const formStyle = {
+  padding: '4rem',
+  backgroundColor: 'white',
+  borderRadius: '20px',
+};
+
+const buttonStyle = { width: '100%' };
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+
+  const isNameValid = (name) => name.length > 2;
+
+  const handleClick = async () => {
+    await createUser({ name: username });
+    navigate('/search');
+  };
+
   return (
-    <Container data-testid="page-login">
-      <h1>Login</h1>
-    </Container>
+    <Div data-testid="page-login">
+      <Row>
+        <Image src={ logo } alt="Logo Trybe Tunes" />
+      </Row>
+      <Row style={ rowStyle }>
+        <Form style={ formStyle }>
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Nome</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Entre com nome"
+              value={ username }
+              onChange={ ({ target }) => setUsername(target.value) }
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="button"
+            style={ buttonStyle }
+            disabled={ !isNameValid(username) }
+            onClick={ handleClick }
+          >
+            Entrar
+          </Button>
+        </Form>
+      </Row>
+    </Div>
   );
 }
