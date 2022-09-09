41e27d240f843e3721dafed3ef6dac341cbf011e
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:50:46 -0300

    feat: create page ProfileEdit

---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function ProfileEdit() {
+  return (
+    <Container data-testid="page-profile-edit">
+      <h1>Profile Edit</h1>
+    </Container>
+  );
+}
