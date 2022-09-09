dbdc78efb2160a9c9ea8d6b7a1fd062a4ba4725f
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:52:11 -0300

    feat: config routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (<p>TrybeTunes</p>);
-  }
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album:id" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/*" element={ <NotFound /> } />
+      </Routes>
+    </div>
+
+  );
 }
-
-export default App;
