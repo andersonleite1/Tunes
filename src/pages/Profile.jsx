e041f2778324bbda0e9ec06c1167643784e00c3e
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-09 08:50:20 -0300

    feat: create page Profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Profile() {
+  return (
+    <Container data-testid="page-profile">
+      <h1>Profile</h1>
+    </Container>
+  );
+}
