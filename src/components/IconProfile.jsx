a0a0f7a10367a2d921089724c9b8045e4494d8b9
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-10 08:04:17 -0300

    feat: create component IconProfile

---
+++ src/components/IconProfile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+import styled from 'styled-components';
+
+import icon from '../images/avatar.png';
+
+const Section = styled.section`
+  margin: auto;
+  width: 12rem;
+  height: 2.3rem;
+  background-color: white;
+  border-radius: 5rem;
+`;
+
+const Avatar = styled.img`
+  width: 2.3rem;
+  max-width: 100%;
+  border-radius: 50%;
+`;
+
+const Name = styled.span`
+  color: #111111;
+  padding-right: 1rem;
+  margin-left: 0.5rem;
+`;
+
+export default function IconProfile({ avatar, username }) {
+  return (
+    <Section>
+      <Avatar src={ !avatar ? icon : avatar } alt="Avatar" />
+      <Name>{ username }</Name>
+    </Section>
+  );
+}
+
+IconProfile.propTypes = {
+  username: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+};
+
+IconProfile.defaultProps = { avatar: undefined };
