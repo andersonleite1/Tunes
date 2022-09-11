98785f7dbca68b882a067d08466e092e773ecab1
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-11 17:31:43 -0300

    feat: create component AlbumNotFound

---
+++ src/components/AlbumNotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h3`
+    height: 40vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #023031;
+    font-size: 2rem;
+    font-weight: 400;
+    letter-spacing: 0.1rem;
+`;
+
+export default function AlbumNotFound() {
+  return (
+    <Title>Nenhum álbum foi encontrado</Title>
+  );
+}
