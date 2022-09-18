b3b1bcf0b44c66bfeb70a83e4dc85011091f3bb8
Anderson Leite <andersonleite.dev@gmail.com>
2022-09-18 11:39:20 -0300

    feat: page profile returns user information

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import ContentText from '../components/ContentText';
 import Header from '../components/Header';
+import icon from '../images/avatar.png';
+
+import { getUser } from '../services/userAPI';
+import AvatarIcon from '../components/AvatarIcon';
+
+import simulateNetworkRequest from '../services/simulateRequest';
+import LoadingCycle from '../components/LoadingCycle';
 
 const Div = styled.div`
-    background-color: #fafafa;
-    width: 100vw;
-    height: 100vh;
-  `;
+  background-color: #fafafa;
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+`;
+
+const Section = styled.section`
+  display: table;
+  margin: auto;
+  margin-top: 5rem;
+`;
+
+const BoxInline = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 20rem;
+  margin-bottom: 2rem;
+`;
 
 export default function Profile() {
+  const [user, setUser] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
+  const fetchUserData = async () => {
+    const userData = await getUser();
+    setUser(userData);
+  };
+
+  useEffect(() => {
+    fetchUserData();
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
+  const navigate = useNavigate();
+
   return (
     <Div>
       <Header />
+      { isLoading ? <LoadingCycle /> : (
+        <Section>
+          <BoxInline>
+            <AvatarIcon url={ !user.image ? icon : user.image } />
+            <Button
+              variant="outline-primary"
+              onClick={ () => navigate('/profile/edit') }
+            >
+              Editar perfil
+            </Button>
+          </BoxInline>
+          <ContentText title="Nome" description={ user.name } />
+          <ContentText title="E-mail" description={ user.email } />
+          <ContentText title="Descrição" description={ user.description } />
+        </Section>
+      )}
     </Div>
   );
 }
